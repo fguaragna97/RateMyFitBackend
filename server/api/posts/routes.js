ffc53580e5c919240a3e1624753035ae140bd89c
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2022-05-29 12:57:55 -0700

    Organize files

---
+++ server/api/posts/routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("./controller");
+
+router.route("/").get(controller.all).post(controller.create);
+
+router
+  .route("/:id")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.delete);
+
+module.exports = router;
