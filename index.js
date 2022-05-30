2715aa83b8305f73396d54f39e8e36e80aa729bd
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2022-05-29 20:31:55 -0700

    Database config

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const http = require("http");
 
 const app = require("./server");
 const config = require("./server/config");
-const port = config.port;
+const database = require("./server/database");
 
+const { dburl, port } = config;
+
+// Connect to the database
+database.connect({ dburl });
+
+// Start http server
 const server = http.createServer(app);
 
 server.listen(port, () => {
--- server/config.js
+++ server/config.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const config = {
   port: process.env.PORT,
+  dburl: process.env.DB_URL,
 };
 
 module.exports = config;
