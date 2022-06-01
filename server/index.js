7809b2a41bd73079792884c711d322cfcff45026
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2022-05-31 20:44:32 -0700

    Cors

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const api = require("./api");
 
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", api);
 
